550a305aa5a2318e50afe216edbfbcf8acc09c8a
Arseny <zinkin.ar@gmail.com>
2024-09-24 20:59:13 +0300

    almost done

--- 674.cpp
+++ 674.cpp
@@ -3,10 +3,30 @@
 using namespace std;
 
 int main() {
-    int n;
-    if (cin >> n && n > 0) { 
-        cout << n << '\n';
-        int** matrix = new()
+    int size;
+    cout << "Введите размер для квадратичной матрицы: "
+    if (cin >> size && size > 0) { 
+
+        int** matrix = new *int[size];
+        for (int i = 0; i < size; i++) {
+            matrix[i] = new int[size];
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                cout << "Введите элемент с индексом " << i << " " << j << ":";
+                while (!(cin >> matrix[i][j])) {
+                    cout << "Ошибка. Проверьте введённые данные\n";
+                }
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                cout << matrix[i][j] << " ";
+            }
+            cout << "\n";
+        }
     } else {
         cout << "Ошибка. Проверьте введённые данные\n";
     }
