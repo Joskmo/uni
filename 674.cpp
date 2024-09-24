1b967823fbc565631b987d718d882480b320b59e
Joskmo <zinkin.ar@gmail.com>
2024-09-24 17:13:35 +0300

    eva task

---
+++ 674.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    if (cin >> n && n > 0) { 
+        cout << n << '\n';
+        int** matrix = new()
+    } else {
+        cout << "Ошибка. Проверьте введённые данные\n";
+    }
+}
